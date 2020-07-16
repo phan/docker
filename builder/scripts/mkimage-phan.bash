03f55a63e0886b0f52c13d0240f52b2f07026100
Tyson Andre <tandre@themeetgroup.com>
2020-07-16 19:19:08 -0400

    Build 3.1.0 with ast 1.0.7

--- builder/scripts/mkimage-phan.bash
+++ builder/scripts/mkimage-phan.bash
@@ -5,7 +5,7 @@
 
 declare REL="${REL:-edge}"
 declare MIRROR="${MIRROR:-http://nl.alpinelinux.org/alpine}"
-declare AST="${AST:-1.0.6}"
+declare AST="${AST:-1.0.7}"
 
 set -eo pipefail; [[ "$TRACE" ]] && set -x
 
